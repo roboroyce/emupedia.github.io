ec18cd08e3be6ce7b945994a081fa6e4b7f105b8
Emupedia <admin@emupedia.net>
2016-03-20 17:47:12 +0200

    More work

--- beta/hooks/postinstall.js
+++ beta/hooks/postinstall.js
@@ -1,40 +1,229 @@
 var fs = require('fs-extra');
+var replace = require('replace-in-file');
 var packages = require('../package.json');
 
 function install(dependency, version) {
 	switch (dependency) {
 		case 'json3':
-			fs.copy('./node_modules/' + dependency +  '/lib/' + dependency + '.min.js', './js/library/' + dependency + '-' + version + '.min.js', function (err) {
-				if (err) {
-					console.error(err);
+			fs.copy('./node_modules/' + dependency +  '/lib/' + dependency + '.min.js', './js/library/' + dependency + '-' + version + '.min.js', function (error) {
+				if (error) {
+					console.error('Error occurred:', error);
 				} else {
 					console.log(dependency + ' version ' + version + ' installed!');
 				}
 			});
 			break;
 		case 'es5-shim':
-			fs.copy('./node_modules/' + dependency +  '/' + dependency + '.min.js', './js/library/' + dependency + '-' + version + '.min.js', function (err) {
-				if (err) {
-					console.error(err);
+			fs.copy('./node_modules/' + dependency +  '/' + dependency + '.min.js', './js/library/' + dependency + '-' + version + '.min.js', function (error) {
+				if (error) {
+					console.error('Error occurred:', error);
 				} else {
-					fs.copy('./node_modules/' + dependency +  '/es5-sham.min.js', './js/library/es5-sham-' + version + '.min.js', function (err) {
-						if (err) {
-							console.error(err);
+					replace({
+						files: './js/library/' + dependency + '-' + version + '.min.js',
+						replace: '//# sourceMappingURL=es5-shim.map',
+						with: '//# sourceMappingURL=es5-shim-' + version + '.map'
+					}, function(error, changedFiles) {
+						if (error) {
+							console.error('Error occurred:', error);
 						} else {
-							console.log(dependency + ' version ' + version + ' installed!');
+							fs.copy('./node_modules/' + dependency +  '/' + dependency + '.map', './js/library/' + dependency + '-' + version + '.map', function (error) {
+								if (error) {
+									console.error('Error occurred:', error);
+								} else {
+									replace({
+										files: './js/library/es5-shim-' + version + '.map',
+										replace: '"sources":["es5-shim.js"]',
+										with: '"sources":["es5-shim-' + version + '.js"]'
+									}, function(error, changedFiles) {
+										if (error) {
+											console.error('Error occurred:', error);
+										} else {
+											fs.copy('./node_modules/' + dependency +  '/es5-shim.js', './js/library/es5-shim-' + version + '.js', function (error) {
+												if (error) {
+													console.error('Error occurred:', error);
+												} else {
+													fs.copy('./node_modules/' + dependency +  '/es5-sham.min.js', './js/library/es5-sham-' + version + '.min.js', function (error) {
+														if (error) {
+															console.error('Error occurred:', error);
+														} else {
+															replace({
+																files: './js/library/es5-sham-' + version + '.min.js',
+																replace: '//# sourceMappingURL=es5-sham.map',
+																with: '//# sourceMappingURL=es5-sham-' + version + '.map'
+															}, function(error, changedFiles) {
+																if (error) {
+																	console.error('Error occurred:', error);
+																} else {
+																	fs.copy('./node_modules/' + dependency +  '/es5-sham.map', './js/library/es5-sham-' + version + '.map', function (error) {
+																		if (error) {
+																			console.error('Error occurred:', error);
+																		} else {
+																			replace({
+																				files: './js/library/es5-sham-' + version + '.map',
+																				replace: '"sources":["es5-sham.js"]',
+																				with: '"sources":["es5-sham-' + version + '.js"]'
+																			}, function(error, changedFiles) {
+																				if (error) {
+																					console.error('Error occurred:', error);
+																				} else {
+																					fs.copy('./node_modules/' + dependency +  '/es5-sham.js', './js/library/es5-sham-' + version + '.js', function (error) {
+																						if (error) {
+																							console.error('Error occurred:', error);
+																						} else {
+																							console.log(dependency + ' version ' + version + ' installed!');
+																						}
+																					});
+																				}
+																			});
+																		}
+																	});
+																}
+															});
+														}
+													});
+												}
+											});
+										}
+									});
+								}
+							});
 						}
 					});
 				}
 			});
 			break;
 		case 'es6-shim':
-			fs.copy('./node_modules/' + dependency +  '/' + dependency + '.min.js', './js/library/' + dependency + '-' + version + '.min.js', function (err) {
-				if (err) {
-					console.error(err);
+			fs.copy('./node_modules/' + dependency +  '/' + dependency + '.min.js', './js/library/' + dependency + '-' + version + '.min.js', function (error) {
+				if (error) {
+					console.error('Error occurred:', error);
 				} else {
-					fs.copy('./node_modules/' + dependency +  '/es6-sham.min.js', './js/library/es6-sham-' + version + '.min.js', function (err) {
-						if (err) {
-							console.error(err);
+					replace({
+						files: './js/library/' + dependency + '-' + version + '.min.js',
+						replace: '//# sourceMappingURL=es6-shim.map',
+						with: '//# sourceMappingURL=es6-shim-' + version + '.map'
+					}, function(error, changedFiles) {
+						if (error) {
+							console.error('Error occurred:', error);
+						} else {
+							fs.copy('./node_modules/' + dependency +  '/' + dependency + '.map', './js/library/' + dependency + '-' + version + '.map', function (error) {
+								if (error) {
+									console.error('Error occurred:', error);
+								} else {
+									replace({
+										files: './js/library/es6-shim-' + version + '.map',
+										replace: '"sources":["es6-shim.js"]',
+										with: '"sources":["es6-shim-' + version + '.js"]'
+									}, function(error, changedFiles) {
+										if (error) {
+											console.error('Error occurred:', error);
+										} else {
+											fs.copy('./node_modules/' + dependency +  '/es6-shim.js', './js/library/es6-shim-' + version + '.js', function (error) {
+												if (error) {
+													console.error('Error occurred:', error);
+												} else {
+													fs.copy('./node_modules/' + dependency +  '/es6-sham.min.js', './js/library/es6-sham-' + version + '.min.js', function (error) {
+														if (error) {
+															console.error('Error occurred:', error);
+														} else {
+															replace({
+																files: './js/library/es6-sham-' + version + '.min.js',
+																replace: '//# sourceMappingURL=es6-sham.map',
+																with: '//# sourceMappingURL=es6-sham-' + version + '.map'
+															}, function(error, changedFiles) {
+																if (error) {
+																	console.error('Error occurred:', error);
+																} else {
+																	fs.copy('./node_modules/' + dependency +  '/es6-sham.map', './js/library/es6-sham-' + version + '.map', function (error) {
+																		if (error) {
+																			console.error('Error occurred:', error);
+																		} else {
+																			replace({
+																				files: './js/library/es6-sham-' + version + '.map',
+																				replace: '"sources":["es6-sham.js"]',
+																				with: '"sources":["es6-sham-' + version + '.js"]'
+																			}, function(error, changedFiles) {
+																				if (error) {
+																					console.error('Error occurred:', error);
+																				} else {
+																					fs.copy('./node_modules/' + dependency +  '/es6-sham.js', './js/library/es6-sham-' + version + '.js', function (error) {
+																						if (error) {
+																							console.error('Error occurred:', error);
+																						} else {
+																							console.log(dependency + ' version ' + version + ' installed!');
+																						}
+																					});
+																				}
+																			});
+																		}
+																	});
+																}
+															});
+														}
+													});
+												}
+											});
+										}
+									});
+								}
+							});
+						}
+					});
+				}
+			});
+			break;
+		case 'es6-module-loader':
+			fs.copy('./node_modules/' + dependency +  '/dist/' + dependency + '-dev.src.js', './js/library/' + dependency + '-' + version + '.min.js', function (error) {
+				if (error) {
+					console.error('Error occurred:', error);
+				} else {
+					console.log(dependency + ' version ' + version + ' installed!');
+				}
+			});
+			break;
+		case 'alameda':
+			fs.copy('./node_modules/' + dependency +  '/' + dependency + '.js', './js/library/' + dependency + '-' + version + '.js', function (error) {
+				if (error) {
+					console.error('Error occurred:', error);
+				} else {
+					console.log(dependency + ' version ' + version + ' installed!');
+				}
+			});
+			break;
+		case 'requirejs':
+			fs.copy('./node_modules/' + dependency +  '/require.js', './js/library/' + dependency + '-' + version + '.js', function (error) {
+				if (error) {
+					console.error('Error occurred:', error);
+				} else {
+					console.log(dependency + ' version ' + version + ' installed!');
+				}
+			});
+			break;
+		case 'requirejs-babel':
+			fs.copy('./node_modules/' + dependency +  '/es6.js', './js/library/' + dependency + '-' + version + '.js', function (error) {
+				if (error) {
+					console.error('Error occurred:', error);
+				} else {
+					console.log(dependency + ' version ' + version + ' installed!');
+				}
+			});
+			break;
+		case 'systemjs':
+			fs.copy('./node_modules/' + dependency +  '/dist/system-csp-production.js', './js/library/' + dependency + '-' + version + '.min.js', function (error) {
+				if (error) {
+					console.error('Error occurred:', error);
+				} else {
+					console.log(dependency + ' version ' + version + ' installed!');
+				}
+			});
+			break;
+		case 'systemjs-plugin-babel':
+			fs.copy('./node_modules/' + dependency +  '/plugin-babel.js', './js/library/systemjs-plugin-babel-' + version + '.min.js', function (error) {
+				if (error) {
+					console.error('Error occurred:', error);
+				} else {
+					fs.copy('./node_modules/' + dependency +  '/systemjs-babel-browser.js', './js/library/systemjs-babel-browser-' + version + '.min.js', function (error) {
+						if (error) {
+							console.error('Error occurred:', error);
 						} else {
 							console.log(dependency + ' version ' + version + ' installed!');
 						}
@@ -42,32 +231,14 @@ function install(dependency, version) {
 				}
 			});
 			break;
-		case 'alameda':
-			fs.copy('./node_modules/' + dependency +  '/' + dependency + '.js', './js/library/' + dependency + '-' + version + '.js', function (err) {
-				if (err) {
-					console.error(err);
-				} else {
-					console.log(dependency + ' version ' + version + ' installed!');
-				}
-			});
-			break;
-		case 'requirejs-babel':
-			fs.copy('./node_modules/' + dependency +  '/es6.js', './js/library/' + dependency + '-' + version + '.js', function (err) {
-				if (err) {
-					console.error(err);
-				} else {
-					console.log(dependency + ' version ' + version + ' installed!');
-				}
-			});
-			break;
 		case 'babel-core':
-			fs.copy('./node_modules/' + dependency +  '/browser.min.js', './js/library/babel-browser-' + version + '.min.js', function (err) {
-				if (err) {
-					console.error(err);
+			fs.copy('./node_modules/' + dependency +  '/browser.min.js', './js/library/babel-browser-' + version + '.min.js', function (error) {
+				if (error) {
+					console.error('Error occurred:', error);
 				} else {
-					fs.copy('./node_modules/' + dependency +  '/browser-polyfill.min.js', './js/library/babel-browser-polyfill-' + version + '.min.js', function (err) {
-						if (err) {
-							console.error(err);
+					fs.copy('./node_modules/' + dependency +  '/browser-polyfill.min.js', './js/library/babel-browser-polyfill-' + version + '.min.js', function (error) {
+						if (error) {
+							console.error('Error occurred:', error);
 						} else {
 							console.log(dependency + ' version ' + version + ' installed!');
 						}
@@ -76,13 +247,13 @@ function install(dependency, version) {
 			});
 			break;
 		case 'jquery':
-			fs.copy('./node_modules/' + dependency + '/dist/' + dependency + '.min.js', './js/library/' + dependency + '-' + version + '.min.js', function (err) {
-				if (err) {
-					console.error(err);
+			fs.copy('./node_modules/' + dependency + '/dist/' + dependency + '.min.js', './js/library/' + dependency + '-' + version + '.min.js', function (error) {
+				if (error) {
+					console.error('Error occurred:', error);
 				} else {
-					fs.copy('./node_modules/' + dependency + '/dist/' + dependency + '.min.map', './js/library/' + dependency + '-' + version + '.min.map', function (err) {
-						if (err) {
-							console.error(err);
+					fs.copy('./node_modules/' + dependency + '/dist/' + dependency + '.min.map', './js/library/' + dependency + '-' + version + '.min.map', function (error) {
+						if (error) {
+							console.error('Error occurred:', error);
 						} else {
 							console.log(dependency + ' version ' + version + ' installed!');
 						}
@@ -91,17 +262,17 @@ function install(dependency, version) {
 			});
 			break;
 		case 'mithril':
-			fs.copy('./node_modules/' + dependency + '/' + dependency + '.min.js', './js/library/' + dependency + '-' + version + '.min.js', function (err) {
-				if (err) {
-					console.error(err);
+			fs.copy('./node_modules/' + dependency + '/' + dependency + '.min.js', './js/library/' + dependency + '-' + version + '.min.js', function (error) {
+				if (error) {
+					console.error('Error occurred:', error);
 				} else {
-					fs.copy('./node_modules/' + dependency + '/' + dependency + '.min.js.map', './js/library/' + dependency + '.min.js.map', function (err) {
-						if (err) {
-							console.error(err);
+					fs.copy('./node_modules/' + dependency + '/' + dependency + '.min.js.map', './js/library/' + dependency + '.min.js.map', function (error) {
+						if (error) {
+							console.error('Error occurred:', error);
 						} else {
-							fs.copy('./node_modules/' + dependency + '/' + dependency + '.js', './js/library/' + dependency + '.js', function (err) {
-								if (err) {
-									console.error(err);
+							fs.copy('./node_modules/' + dependency + '/' + dependency + '.js', './js/library/' + dependency + '.js', function (error) {
+								if (error) {
+									console.error('Error occurred:', error);
 								} else {
 									console.log(dependency + ' version ' + version + ' installed!');
 								}
