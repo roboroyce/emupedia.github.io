8d4a723374e1ef2176f137e4d8de1e61cd48b9e9
Emupedia <admin@emupedia.net>
2018-08-21 05:16:04 +0300

    More work

--- beta/js/i8086/cpu.js
+++ beta/js/i8086/cpu.js
@@ -114,137 +114,161 @@ n ^= mask;
 		self.RAM32 = new Uint32Array(self.BRAM);
 
 		self.R = {
+			INDEX: {
+				AL: 0x00,
+				AH: 0x01,
+				BL: 0x02,
+				BH: 0x03,
+				CL: 0x04,
+				CH: 0x05,
+				DL: 0x06,
+				DH: 0x07,
+				AX: 0x00,
+				BX: 0x02,
+				CX: 0x04,
+				DX: 0x06,
+				CS: 0x08,
+				DS: 0x0A,
+				ES: 0x0C,
+				SS: 0x0E,
+				SI: 0x10,
+				DI: 0x12,
+				BP: 0x14,
+				SP: 0x16,
+				IP: 0x18,
+				F:	0x1A
+			},
 			get AL() {
-				return self.S.getUint8(0);
+				return self.S.getUint8(this.INDEX.AL);
 			},
 			set AL(val) {
-				self.S.setUint8(0, val);
+				self.S.setUint8(this.INDEX.AL, val);
 			},
 			get AH() {
-				return self.S.getUint8(1);
+				return self.S.getUint8(this.INDEX.AH);
 			},
 			set AH(val) {
-				self.S.setUint8(1, val);
-			},
-			get AX() {
-				return self.S.getUint16(0, true);
-			},
-			set AX(val) {
-				self.S.setUint16(0, val, true);
+				self.S.setUint8(this.INDEX.AH, val);
 			},
 			get BL() {
-				return self.S.getUint8(2);
+				return self.S.getUint8(this.INDEX.BL);
 			},
 			set BL(val) {
-				self.S.setUint8(2, val);
+				self.S.setUint8(this.INDEX.BL, val);
 			},
 			get BH() {
-				return self.S.getUint8(3);
+				return self.S.getUint8(this.INDEX.BH);
 			},
 			set BH(val) {
-				self.S.setUint8(3, val);
-			},
-			get BX() {
-				return self.S.getUint16(2, true);
-			},
-			set BX(val) {
-				self.S.setUint16(2, val, true);
+				self.S.setUint8(this.INDEX.BH, val);
 			},
 			get CL() {
-				return self.S.getUint8(4);
+				return self.S.getUint8(this.INDEX.CL);
 			},
 			set CL(val) {
-				self.S.setUint8(4, val);
+				self.S.setUint8(this.INDEX.CL, val);
 			},
 			get CH() {
-				return self.S.getUint8(5);
+				return self.S.getUint8(this.INDEX.CH);
 			},
 			set CH(val) {
-				self.S.setUint8(5, val);
-			},
-			get CX() {
-				return self.S.getUint16(4, true);
-			},
-			set CX(val) {
-				self.S.setUint16(4, val, true);
+				self.S.setUint8(this.INDEX.CH, val);
 			},
 			get DL() {
-				return self.S.getUint8(6);
+				return self.S.getUint8(this.INDEX.DL);
 			},
 			set DL(val) {
-				self.S.setUint8(6, val);
+				self.S.setUint8(this.INDEX.DL, val);
 			},
 			get DH() {
-				return self.S.getUint8(7);
+				return self.S.getUint8(this.INDEX.DH);
 			},
 			set DH(val) {
-				self.S.setUint8(7, val);
+				self.S.setUint8(this.INDEX.DH, val);
+			},
+			get AX() {
+				return self.S.getUint16(this.INDEX.AX, true);
+			},
+			set AX(val) {
+				self.S.setUint16(this.INDEX.AX, val, true);
+			},
+			get BX() {
+				return self.S.getUint16(this.INDEX.BX, true);
+			},
+			set BX(val) {
+				self.S.setUint16(this.INDEX.BX, val, true);
+			},
+			get CX() {
+				return self.S.getUint16(this.INDEX.CX, true);
+			},
+			set CX(val) {
+				self.S.setUint16(this.INDEX.CX, val, true);
 			},
 			get DX() {
-				return self.S.getUint16(6, true);
+				return self.S.getUint16(this.INDEX.DX, true);
 			},
 			set DX(val) {
-				self.S.setUint16(6, val, true);
+				self.S.setUint16(this.INDEX.DX, val, true);
 			},
 			get CS() {
-				return self.S.getUint16(8, true);
+				return self.S.getUint16(this.INDEX.CS, true);
 			},
 			set CS(val) {
-				self.S.setUint16(8, val, true);
+				self.S.setUint16(this.INDEX.CS, val, true);
 			},
 			get DS() {
-				return self.S.getUint16(10, true);
+				return self.S.getUint16(this.INDEX.DS, true);
 			},
 			set DS(val) {
-				self.S.setUint16(10, val, true);
+				self.S.setUint16(this.INDEX.DS, val, true);
 			},
 			get ES() {
-				return self.S.getUint16(12, true);
+				return self.S.getUint16(this.INDEX.ES, true);
 			},
 			set ES(val) {
-				self.S.setUint16(12, val, true);
+				self.S.setUint16(this.INDEX.ES, val, true);
 			},
 			get SS() {
-				return self.S.getUint16(14, true);
+				return self.S.getUint16(this.INDEX.SS, true);
 			},
 			set SS(val) {
-				self.S.setUint16(14, val, true);
+				self.S.setUint16(this.INDEX.SS, val, true);
 			},
 			get SI() {
-				return self.S.getUint16(16, true);
+				return self.S.getUint16(this.INDEX.SI, true);
 			},
 			set SI(val) {
-				self.S.setUint16(16, val, true);
+				self.S.setUint16(this.INDEX.SI, val, true);
 			},
 			get DI() {
-				return self.S.getUint16(18, true);
+				return self.S.getUint16(this.INDEX.DI, true);
 			},
 			set DI(val) {
-				self.S.setUint16(18, val, true);
+				self.S.setUint16(this.INDEX.DI, val, true);
 			},
 			get BP() {
-				return self.S.getUint16(20, true);
+				return self.S.getUint16(this.INDEX.BP, true);
 			},
 			set BP(val) {
-				self.S.setUint16(20, val, true);
+				self.S.setUint16(this.INDEX.BP, val, true);
 			},
 			get SP() {
-				return self.S.getUint16(22, true);
+				return self.S.getUint16(this.INDEX.SP, true);
 			},
 			set SP(val) {
-				self.S.setUint16(22, val, true);
+				self.S.setUint16(this.INDEX.SP, val, true);
 			},
 			get IP() {
-				return self.S.getUint16(24, true);
+				return self.S.getUint16(this.INDEX.IP, true);
 			},
 			set IP(val) {
-				self.S.setUint16(24, val, true);
+				self.S.setUint16(this.INDEX.IP, val, true);
 			},
 			get F() {
-				return self.S.getUint16(26, true);
+				return self.S.getUint16(this.INDEX.F, true);
 			},
 			set F(val) {
-				self.S.setUint16(26, val, true);
+				self.S.setUint16(this.INDEX.F, val, true);
 			}
 		};
 		self.R8 = {
@@ -448,100 +472,100 @@ n ^= mask;
 				BIT15:	0x8000
 			},
 			get CF() {
-				return (self.S.getUint16(26, true) & this.MASK.CF) >>> this.INDEX.CF;
+				return (self.S.getUint16(self.R.INDEX.F, true) & this.MASK.CF) >>> this.INDEX.CF;
 			},
 			set CF(val) {
-				self.S.setUint16(26, val ? (this.F | this.MASK.CF) : (this.F & ~this.MASK.CF), true);
+				self.S.setUint16(self.R.INDEX.F, val ? (self.R.F | this.MASK.CF) : (self.R.F & ~this.MASK.CF), true);
 			},
 			get BIT1() {
-				return (self.S.getUint16(26, true) & this.MASK.BIT1) >>> this.INDEX.BIT1;
+				return (self.S.getUint16(self.R.INDEX.F, true) & this.MASK.BIT1) >>> this.INDEX.BIT1;
 			},
 			set BIT1(val) {
-				self.S.setUint16(26, val ? (this.F | this.MASK.BIT1) : (this.F & ~this.MASK.BIT1), true);
+				self.S.setUint16(self.R.INDEX.F, val ? (self.R.F | this.MASK.BIT1) : (self.R.F & ~this.MASK.BIT1), true);
 			},
 			get PF() {
-				return (self.S.getUint16(26, true) & this.MASK.PF) >>> this.INDEX.PF;
+				return (self.S.getUint16(self.R.INDEX.F, true) & this.MASK.PF) >>> this.INDEX.PF;
 			},
 			set PF(val) {
-				self.S.setUint16(26, val ? (this.F | this.MASK.PF) : (this.F & ~this.MASK.PF), true);
+				self.S.setUint16(self.R.INDEX.F, val ? (self.R.F | this.MASK.PF) : (self.R.F & ~this.MASK.PF), true);
 			},
 			get BIT3() {
-				return (self.S.getUint16(26, true) & this.MASK.BIT3) >>> this.INDEX.BIT3;
+				return (self.S.getUint16(self.R.INDEX.F, true) & this.MASK.BIT3) >>> this.INDEX.BIT3;
 			},
 			set BIT3(val) {
-				self.S.setUint16(26, val ? (this.F | this.MASK.BIT3) : (this.F & ~this.MASK.BIT3), true);
+				self.S.setUint16(self.R.INDEX.F, val ? (self.R.F | this.MASK.BIT3) : (self.R.F & ~this.MASK.BIT3), true);
 			},
 			get AF() {
-				return (self.S.getUint16(26, true) & this.MASK.AF) >>> this.INDEX.AF;
+				return (self.S.getUint16(self.R.INDEX.F, true) & this.MASK.AF) >>> this.INDEX.AF;
 			},
 			set AF(val) {
-				self.S.setUint16(26, val ? (this.F | this.MASK.AF) : (this.F & ~this.MASK.AF), true);
+				self.S.setUint16(self.R.INDEX.F, val ? (self.R.F | this.MASK.AF) : (self.R.F & ~this.MASK.AF), true);
 			},
 			get BIT5() {
-				return (self.S.getUint16(26, true) & this.MASK.BIT5) >>> this.INDEX.BIT5;
+				return (self.S.getUint16(self.R.INDEX.F, true) & this.MASK.BIT5) >>> this.INDEX.BIT5;
 			},
 			set BIT5(val) {
-				self.S.setUint16(26, val ? (this.F | this.MASK.BIT5) : (this.F & ~this.MASK.BIT5), true);
+				self.S.setUint16(self.R.INDEX.F, val ? (self.R.F | this.MASK.BIT5) : (self.R.F & ~this.MASK.BIT5), true);
 			},
 			get ZF() {
-				return (self.S.getUint16(26, true) & this.MASK.ZF) >>> this.INDEX.ZF;
+				return (self.S.getUint16(self.R.INDEX.F, true) & this.MASK.ZF) >>> this.INDEX.ZF;
 			},
 			set ZF(val) {
-				self.S.setUint16(26, val ? (this.F | this.MASK.ZF) : (this.F & ~this.MASK.ZF), true);
+				self.S.setUint16(self.R.INDEX.F, val ? (self.R.F | this.MASK.ZF) : (self.R.F & ~this.MASK.ZF), true);
 			},
 			get SF() {
-				return (self.S.getUint16(26, true) & this.MASK.SF) >>> this.INDEX.SF;
+				return (self.S.getUint16(self.R.INDEX.F, true) & this.MASK.SF) >>> this.INDEX.SF;
 			},
 			set SF(val) {
-				self.S.setUint16(26, val ? (this.F | this.MASK.SF) : (this.F & ~this.MASK.SF), true);
+				self.S.setUint16(self.R.INDEX.F, val ? (self.R.F | this.MASK.SF) : (self.R.F & ~this.MASK.SF), true);
 			},
 			get TF() {
-				return (self.S.getUint16(26, true) & this.MASK.TF) >>> this.INDEX.TF;
+				return (self.S.getUint16(self.R.INDEX.F, true) & this.MASK.TF) >>> this.INDEX.TF;
 			},
 			set TF(val) {
-				self.S.setUint16(26, val ? (this.F | this.MASK.TF) : (this.F & ~this.MASK.TF), true);
+				self.S.setUint16(self.R.INDEX.F, val ? (self.R.F | this.MASK.TF) : (self.R.F & ~this.MASK.TF), true);
 			},
 			get IF() {
-				return (self.S.getUint16(26, true) & this.MASK.IF) >>> this.INDEX.IF;
+				return (self.S.getUint16(self.R.INDEX.F, true) & this.MASK.IF) >>> this.INDEX.IF;
 			},
 			set IF(val) {
-				self.S.setUint16(26, val ? (this.F | this.MASK.IF) : (this.F & ~this.MASK.IF), true);
+				self.S.setUint16(self.R.INDEX.F, val ? (self.R.F | this.MASK.IF) : (self.R.F & ~this.MASK.IF), true);
 			},
 			get DF() {
-				return (self.S.getUint16(26, true) & this.MASK.DF) >>> this.INDEX.DF;
+				return (self.S.getUint16(self.R.INDEX.F, true) & this.MASK.DF) >>> this.INDEX.DF;
 			},
 			set DF(val) {
-				self.S.setUint16(26, val ? (this.F | this.MASK.DF) : (this.F & ~this.MASK.DF), true);
+				self.S.setUint16(self.R.INDEX.F, val ? (self.R.F | this.MASK.DF) : (self.R.F & ~this.MASK.DF), true);
 			},
 			get OF() {
-				return (self.S.getUint16(26, true) & this.MASK.OF) >>> this.INDEX.OF;
+				return (self.S.getUint16(self.R.INDEX.F, true) & this.MASK.OF) >>> this.INDEX.OF;
 			},
 			set OF(val) {
-				self.S.setUint16(26, val ? (this.F | this.MASK.OF) : (this.F & ~this.MASK.OF), true);
+				self.S.setUint16(self.R.INDEX.F, val ? (self.R.F | this.MASK.OF) : (self.R.F & ~this.MASK.OF), true);
 			},
 			get BIT12() {
-				return (self.S.getUint16(26, true) & this.MASK.BIT12) >>> this.INDEX.BIT12;
+				return (self.S.getUint16(self.R.INDEX.F, true) & this.MASK.BIT12) >>> this.INDEX.BIT12;
 			},
 			set BIT12(val) {
-				self.S.setUint16(26, val ? (this.F | this.MASK.BIT12) : (this.F & ~this.MASK.BIT12), true);
+				self.S.setUint16(self.R.INDEX.F, val ? (self.R.F | this.MASK.BIT12) : (self.R.F & ~this.MASK.BIT12), true);
 			},
 			get BIT13() {
-				return (self.S.getUint16(26, true) & this.MASK.BIT13) >>> this.INDEX.BIT13;
+				return (self.S.getUint16(self.R.INDEX.F, true) & this.MASK.BIT13) >>> this.INDEX.BIT13;
 			},
 			set BIT13(val) {
-				self.S.setUint16(26, val ? (this.F | this.MASK.BIT13) : (this.F & ~this.MASK.BIT13), true);
+				self.S.setUint16(self.R.INDEX.F, val ? (self.R.F | this.MASK.BIT13) : (self.R.F & ~this.MASK.BIT13), true);
 			},
 			get BIT14() {
-				return (self.S.getUint16(26, true) & this.MASK.BIT14) >>> this.INDEX.BIT14;
+				return (self.S.getUint16(self.R.INDEX.F, true) & this.MASK.BIT14) >>> this.INDEX.BIT14;
 			},
 			set BIT14(val) {
-				self.S.setUint16(26, val ? (this.F | this.MASK.BIT14) : (this.F & ~this.MASK.BIT14), true);
+				self.S.setUint16(self.R.INDEX.F, val ? (self.R.F | this.MASK.BIT14) : (self.R.F & ~this.MASK.BIT14), true);
 			},
 			get BIT15() {
-				return (self.S.getUint16(26, true) & this.MASK.BIT15) >>> this.INDEX.BIT15;
+				return (self.S.getUint16(self.R.INDEX.F, true) & this.MASK.BIT15) >>> this.INDEX.BIT15;
 			},
 			set BIT15(val) {
-				self.S.setUint16(26, val ? (this.F | this.MASK.BIT15) : (this.F & ~this.MASK.BIT15), true);
+				self.S.setUint16(self.R.INDEX.F, val ? (self.R.F | this.MASK.BIT15) : (self.R.F & ~this.MASK.BIT15), true);
 			}
 		};
 	}
