52d6d17ee44457c47957e7e6ad6d2b48af138d91
Emupedia <admin@emupedia.net>
2019-04-01 05:01:58 +0300

    More work

--- beta/emuos/js/components/x-frame-bypass.js
+++ beta/emuos/js/components/x-frame-bypass.js
@@ -1,82 +1,106 @@
 customElements.define('x-frame-bypass', class extends HTMLIFrameElement {
-	constructor () {
-		super()
+	constructor() {
+		super();
 	}
-	connectedCallback () {
-		this.load(this.src)
-		this.src = ''
-		this.sandbox = '' + this.sandbox || 'allow-forms allow-modals allow-pointer-lock allow-popups allow-popups-to-escape-sandbox allow-presentation allow-same-origin allow-scripts allow-top-navigation-by-user-activation' // all except allow-top-navigation
+
+	connectedCallback() {
+		this.load(this.src);
+		this.src = '';
+		this.sandbox = '' + this.sandbox || 'allow-forms allow-modals allow-pointer-lock allow-popups allow-popups-to-escape-sandbox allow-presentation allow-same-origin allow-scripts allow-top-navigation-by-user-activation'; // all except allow-top-navigation
 	}
-	load (url, options) {
-		if (!url || !url.startsWith('http'))
-			throw new Error(`X-Frame-Bypass src ${url} does not start with http(s)://`)
-		console.log('X-Frame-Bypass loading:', url)
-		this.srcdoc = `<html>
-<head>
-	<style>
-	.loader {
-		position: absolute;
-		top: calc(50% - 25px);
-		left: calc(50% - 25px);
-		width: 50px;
-		height: 50px;
-		background-color: #333;
-		border-radius: 50%;  
-		animation: loader 1s infinite ease-in-out;
-	}
-	@keyframes loader {
-		0% {
-		transform: scale(0);
+
+	load(url, options) {
+		if (!url || !url.startsWith('http')) {
+			throw new Error(`X-Frame-Bypass src ${url} does not start with http(s)://`);
 		}
-		100% {
-		transform: scale(1);
-		opacity: 0;
+
+		console.log('X-Frame-Bypass loading:', url);
+
+		// noinspection HtmlRequiredTitleElement
+		this.srcdoc = `<html lang="en">
+	<head>
+		<style>
+	
+		.loader {
+			position: absolute;
+			top: calc(50% - 25px);
+			left: calc(50% - 25px);
+			width: 50px;
+			height: 50px;
+			background-color: #333;
+			border-radius: 50%;  
+			animation: loader 1s infinite ease-in-out;
 		}
-	}
-	</style>
-</head>
-<body>
-	<div class="loader"></div>
-</body>
-</html>`
+	
+		@keyframes loader {
+			0% {
+				transform: scale(0);
+			}
+			100% {
+				transform: scale(1);
+				opacity: 0;
+			}
+		}
+		</style>
+	</head>
+	<body>
+		<div class="loader"></div>
+	</body>
+</html>`;
+
 		this.fetchProxy(url, options, 0).then(res => res.text()).then(data => {
-			if (data)
+			if (data) {
+				url = url.split('#')[0];
+				// noinspection HtmlRequiredTitleElement,JSUnresolvedVariable
 				this.srcdoc = data.replace(/<head([^>]*)>/i, `<head$1>
 	<base href="${url}">
 	<script>
 	// X-Frame-Bypass navigation event handlers
-	document.addEventListener('click', e => {
+	document.addEventListener('click', function(e) {
 		if (frameElement && document.activeElement && document.activeElement.href) {
-			e.preventDefault()
+			e.preventDefault();
+			
 			frameElement.load(document.activeElement.href)
 		}
-	})
-	document.addEventListener('submit', e => {
+	});
+
+	document.addEventListener('submit', function(e) {
 		if (frameElement && document.activeElement && document.activeElement.form && document.activeElement.form.action) {
-			e.preventDefault()
-			if (document.activeElement.form.method === 'post')
-				frameElement.load(document.activeElement.form.action, {method: 'post', body: new FormData(document.activeElement.form)})
-			else
+			e.preventDefault();
+			
+			if (document.activeElement.form.method === 'post') {
+				frameElement.load(document.activeElement.form.action, {method: 'post', body: new FormData(document.activeElement.form)});
+			} else {
 				frameElement.load(document.activeElement.form.action + '?' + new URLSearchParams(new FormData(document.activeElement.form)))
+			}
 		}
-	})
-	</script>`)
-		}).catch(e => console.error('Cannot load X-Frame-Bypass:', e))
+	});
+	</script>`);
+			this.srcdoc = data.replace('href="/', 'href="' + url);
+			this.srcdoc = data.replace('src="/', 'src="' + url);
+		}
+		}).catch(e => console.error('Cannot load X-Frame-Bypass:', e));
 	}
-	fetchProxy (url, options, i) {
+
+	fetchProxy(url, options, i) {
 		const proxy = [
 			'https://cors.io/?',
 			'https://jsonp.afeld.me/?url=',
 			'https://cors-anywhere.herokuapp.com/'
-		]
+		];
+
 		return fetch(proxy[i] + url, options).then(res => {
-			if (!res.ok)
+			if (!res.ok) {
 				throw new Error(`${res.status} ${res.statusText}`);
+			}
+
 			return res
 		}).catch(error => {
-			if (i === proxy.length - 1)
-				throw error
+			if (i === proxy.length - 1) {
+				throw error;
+			}
+
 			return this.fetchProxy(url, options, i + 1)
 		})
 	}
-}, {extends: 'iframe'})
+}, {extends: 'iframe'});
