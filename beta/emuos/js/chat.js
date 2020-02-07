566703b2d4efda1c9b01c1c7f8d2b8982a904098
Emupedia <admin@emupedia.net>
2020-02-07 10:23:01 +0200

    More work

--- beta/emuos/js/chat.js
+++ beta/emuos/js/chat.js
@@ -277,9 +277,13 @@
 			// noinspection JSUnusedAssignment
 			var sl = 0;
 			var fl = 0;
+			// noinspection JSUnusedAssignment
 			var f = '';
+			// noinspection JSUnusedAssignment
 			var r = '';
+			// noinspection JSUnusedAssignment
 			var s = '';
+			// noinspection JSUnusedAssignment
 			var ra = '';
 			var otemp = '';
 			var oi = '';
