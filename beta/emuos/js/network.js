6f55103a37edc83adf20063ee35b49940c6c2a8a
Emupedia <admin@emupedia.net>
2020-02-07 04:51:45 +0200

    More work

--- beta/emuos/js/network.js
+++ beta/emuos/js/network.js
@@ -142,7 +142,7 @@
 			var server = config.servers[config.server];
 
 			client = {
-				socket: io.connect(server, {transports: ['websocket'], secure: true}),
+				socket: io.connect(server, {transports: ['websocket'], secure: true, rejectUnauthorized: false}),
 				config: config,
 				server: server,
 				preload: {}
