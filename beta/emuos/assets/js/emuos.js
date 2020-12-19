1d0fe7847f53f2576d50e1994ef0538199dbba37
Emupedia <admin@emupedia.net>
2020-12-19 08:55:46 +0200

    More work

--- beta/emuos/assets/js/emuos.js
+++ beta/emuos/assets/js/emuos.js
@@ -662,8 +662,8 @@
 
 		if (typeof self.options.network.start === 'function') {
 			window['NETWORK_CONNECTION'] = self.options.network.start({
-				servers: ['wss://ws.emupedia.net/ws/', 'wss://ws.emuos.net/ws/', 'wss://ws.emuos.org/ws/'],
-				server: ~window.location.hostname.indexOf('emuos.org') ? 2 : (~window.location.hostname.indexOf('emuos.net') ? 1 : 0),
+				servers: ['wss://ws.emupedia.net/ws/', 'wss://ws.emupedia.org/ws/', 'wss://ws.emuos.net/ws/', 'wss://ws.emuos.org/ws/'],
+				server: ~window.location.hostname.indexOf('emupedia.net') ? 0 : (~window.location.hostname.indexOf('emupedia.org') ? 1 : (~window.location.hostname.indexOf('emuos.net') ? 2 : (~window.location.hostname.indexOf('emuos.org') ? 3 : 0))),
 				mode: 0
 			});
 		}
