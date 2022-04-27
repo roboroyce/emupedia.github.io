40bd189ea2ac427fbd4b9d427b32cbd147424c4c
Emupedia <admin@emupedia.net>
2022-04-27 13:08:31 +0300

    More work

--- emupedia-app-emuchat/js/main.js
+++ emupedia-app-emuchat/js/main.js
@@ -1,13 +1,15 @@
 // noinspection ThisExpressionReferencesGlobalObjectJS,JSUnusedLocalSymbols,DuplicatedCode
 (function(global) {
+	var location = ~window.location.hostname.indexOf('emupedia.net') ? 'emupedia.net' : (~window.location.hostname.indexOf('emupedia.org') ? 'emupedia.org' : (~window.location.hostname.indexOf('emuos.net') ? 'emuos.net' : (~window.location.hostname.indexOf('emuos.org') ? 'emuos.org' : 'emupedia.net')))
+
 	// noinspection JSFileReferences
 	requirejs.config({
 		paths: {
 			bson: '../../beta/emuos/assets/js/libraries/bson-4.5.2',
 			jquery: '../../beta/emuos/assets/js/libraries/jquery-3.6.0.min',
 			'jquery-ajax-retry': '../../beta/emuos/assets/js/libraries/jquery-ajax-retry-0.2.8.min',
-			network: '../../beta/emuos/assets/js/network',
-			socket: '../../beta/emuos/assets/js/socket',
+			network: '//ws.' + location + '/server/app/network_iframe',
+			socket: '//ws.' + location + '/server/app/u_socket_es5',
 			simplestorage: '../../beta/emuos/assets/js/libraries/simplestorage-0.2.1.min'
 		},
 		shim: {
@@ -36,20 +38,18 @@
 		'network'
 	], function($, network) {
 		$(function() {
-			var servers = ['wss://ws.emupedia.net/ws/', 'wss://ws.emupedia.net/ws/', 'wss://ws.emupedia.org/ws/', 'wss://ws.emupedia.org/ws/', 'wss://ws.emuos.net/ws/', 'wss://ws.emuos.net/ws/', 'wss://ws.emuos.org/ws/', 'wss://ws.emuos.org/ws/', 'ws://cojmar.ddns.net/ws/'];
-			var domains = ['emupedia.net', 'emuchat.emupedia.net', 'emupedia.org', 'emuchat.emupedia.org', 'emuos.net', 'emuchat.emuos.net', 'emuos.org', 'emuchat.emuos.org', 'cojmar.ddns.net'];
-			var frontend = ~domains.indexOf(window.location.hostname) ? 'https://emuchat.' + domains[domains.indexOf(window.location.hostname)] + '/' : 'https://emuchat.emupedia.net/';
+			var servers = ['wss://ws.emupedia.net/ws/', 'wss://ws.emupedia.org/ws/', 'wss://ws.emuos.net/ws/', 'wss://ws.emuos.org/ws/'];
 
 			// noinspection DuplicatedCode
 			window['NETWORK_CONNECTION'] = network.start({
 				servers: servers,
-				server: ~domains.indexOf(window.location.hostname) ? domains.indexOf(window.location.hostname) : 0,
+				server: ~window.location.hostname.indexOf('emupedia.net') ? 0 : (~window.location.hostname.indexOf('emupedia.org') ? 1 : (~window.location.hostname.indexOf('emuos.net') ? 2 : (~window.location.hostname.indexOf('emuos.org') ? 3 : 0))),
 				mode: 0
 			});
 
 			setTimeout(function() {
 				// noinspection HtmlDeprecatedAttribute
-				$('body').append('<iframe id="emuchat" width="100%" height="100%" src="' + frontend + '" frameborder="0" allowFullscreen="allowFullscreen" allowTransparency="true" allow="autoplay; fullscreen; accelerometer; gyroscope; geolocation; microphone; camera; midi; encrypted-media; clipboard-read; clipboard-write" sandbox="allow-forms allow-downloads allow-modals allow-pointer-lock allow-popups allow-popups-to-escape-sandbox allow-presentation allow-same-origin allow-scripts allow-top-navigation-by-user-activation"></iframe>');
+				$('body').append('<iframe id="emuchat" width="100%" height="100%" src="https://emuchat.' + location + '" frameborder="0" allowFullscreen="allowFullscreen" allowTransparency="true" allow="autoplay; fullscreen; accelerometer; gyroscope; geolocation; microphone; camera; midi; encrypted-media; clipboard-read; clipboard-write" sandbox="allow-forms allow-downloads allow-modals allow-pointer-lock allow-popups allow-popups-to-escape-sandbox allow-presentation allow-same-origin allow-scripts allow-top-navigation-by-user-activation"></iframe>');
 
 				// noinspection DuplicatedCode
 				$(document).find('iframe').first().off('load').on('load', function() {
