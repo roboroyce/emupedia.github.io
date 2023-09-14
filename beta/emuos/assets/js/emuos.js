63fb5dda89b3da94834c9effdff5094126f786d1
Emupedia <admin@emupedia.net>
2023-09-15 00:04:39 +0300

    More work

--- beta/emuos/assets/js/emuos.js
+++ beta/emuos/assets/js/emuos.js
@@ -943,11 +943,13 @@
 
 		if (typeof self.options.network !== 'undefined') {
 			if (typeof self.options.network.start === 'function') {
-				window['NETWORK_CONNECTION'] = self.options.network.start({
-					servers: ['wss://ws.emupedia.net/ws/', 'wss://ws.emupedia.org/ws/', 'wss://ws.emuos.net/ws/', 'wss://ws.emuos.org/ws/'],
-					server: ~window.location.hostname.indexOf('emupedia.net') ? 0 : (~window.location.hostname.indexOf('emupedia.org') ? 1 : (~window.location.hostname.indexOf('emuos.net') ? 2 : (~window.location.hostname.indexOf('emuos.org') ? 3 : 0))),
-					mode: 0
-				});
+				setTimeout(function() {
+					window['NETWORK_CONNECTION'] = self.options.network.start({
+						servers: ['wss://ws.emupedia.net/ws/', 'wss://ws.emupedia.org/ws/', 'wss://ws.emuos.net/ws/', 'wss://ws.emuos.org/ws/'],
+						server: ~window.location.hostname.indexOf('emupedia.net') ? 0 : (~window.location.hostname.indexOf('emupedia.org') ? 1 : (~window.location.hostname.indexOf('emuos.net') ? 2 : (~window.location.hostname.indexOf('emuos.org') ? 3 : 0))),
+						mode: 0
+					});
+				}, 3000);
 			}
 		}
 
